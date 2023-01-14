880afad2cf0fbcc68671018a0a0e1e0d7bc06606
Nissan Kumar <nissankumar9001@gmail.com>
2023-01-14 19:38:49 +0530

    installed tailwind in decyber-frontend

--- frontend/decyber-frontend/src/App.js
+++ frontend/decyber-frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>DeCyber</h1>
+      <h1 className=' bg-slate-600 text-red-500'>DeCyber</h1>
     </div>
   );
 }
